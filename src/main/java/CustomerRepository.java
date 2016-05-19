1c5823a37ac5c66150bfe0351e3a621f7c60867f
virvirlopez <virbyte@gmail.com>
2016-05-19 17:55:53 +0200

    push

---
+++ src/main/java/CustomerRepository.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    List<Customer> findByFirstName(String firstName);
+
+}
